c2a737f70d64b48a7031ef4460caaa35d9d623b0
André Torres <150715327+andtors@users.noreply.github.com>
2024-10-24 16:58:12 -0300

    SC-6 Concluida

---
+++ SC-6 (Exercicio)/Funcionarios.sql
@@ -0,0 +1,46 @@
+/* Exercicio da secão 6 */
+CREATE DATABASE EXERCICIO;
+CREATE TABLE FUNCIONARIOS(
+      idFuncionario integer,
+      nome varchar(100),
+      email varchar(200),
+      sexo varchar(10),
+      departamento varchar(100),
+      admissao varchar(10),
+      salario integer,
+      cargo varchar(100),
+      idRegiao int
+);
+insert into funcionarios values (1,'Kelley','rkelley0@soundcloud.com','Feminino','Computadores','10/2/2009',67470,'Structural Engineer',2);
+insert into funcionarios values (2,'Armstrong','sarmstrong1@infoseek.co.jp','Masculino','Esporte','3/31/2008',71869,'Financial Advisor',2);
+insert into funcionarios values (3,'Carr','fcarr2@woothemes.com','Masculino','Automotivo','7/12/2009',101768,'Recruiting Manager',3);
+insert into funcionarios values (4,'Murray','jmurray3@gov.uk','Feminino','Joalheria','12/25/2014',96897,'Desktop Support Technician',3);
+insert into funcionarios values (5,'Ellis','jellis4@sciencedirect.com','Feminino','Alimentícios','9/19/2002',63702,'Software Engineer III',7);
+insert into funcionarios values (6,'Phillips','bphillips5@time.com','Masculino','Ferramentas','8/21/2013',118497,'Executive Secretary',1);
+insert into funcionarios values (7,'Williamson','rwilliamson6@ted.com','Masculino','Computadores','5/14/2006',65889,'Dental Hygienist',6);
+insert into funcionarios values (8,'Harris','aharris7@ucoz.com','Feminino','Brinquedos','8/12/2003',84427,'Safety Technician I',4);
+insert into funcionarios values (9,'James','rjames8@prnewswire.com','Masculino','Joalheria','9/7/2005',108657,'Sales Associate',2);
+/* e demais registros...*/
+
+
+/* 1 */
+/* Quantidade */
+SELECT 
+    (SELECT COUNT(*) FROM FUNCIONARIOS WHERE DEPARTAMENTO = 'Filmes') AS QNTD_FILMES,
+    (SELECT COUNT(*) FROM FUNCIONARIOS WHERE DEPARTAMENTO = 'roupas') AS QNTD_ROUPAS;
+
+/* Funcionarios */
+/* Quantidade */
+SELECT * FROM FUNCIONARIOS WHERE DEPARTAMENTO = 'Filmes' OR DEPARTAMENTO = 'Roupas';
+
+/* 2 */
+/* Quantidade*/
+SELECT COUNT(*) AS QNTD_FEM FROM FUNCIONARIOS  WHERE SEXO = 'Feminino' group by SEXO;
+
+/* Funcionarios */
+SELECT * FROM FUNCIONARIOS WHERE (SEXO = 'Feminino' AND DEPARTAMENTO = 'Filmes') OR (SEXO = 'Feminino' AND DEPARTAMENTO = 'Lar');
+
+/* 3 */
+/* Funcionarios */
+SELECT * FROM FUNCIONARIOS WHERE SEXO = 'MASCULINO' OR DEPARTAMENTO = 'Jardim';
+
