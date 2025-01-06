82ab9d4d8f2f2af49c517dd4d45fe71628ee3c37
André Torres <150715327+andtors@users.noreply.github.com>
2025-01-05 21:17:47 -0300

    SC-30-124

---
+++ SC-30/Dateset.sql
@@ -0,0 +1,10 @@
+/* MOSTRAR O PADRAO DE DATA */
+
+SHOW DATESTYLE
+
+/* PARA ALTERAR O PADRÃO DE DATA BASTA IR NO DIRETORIO DO POSTGRESQL ->
+    DATA -> postgresql.config -> datestyle e alterar para d - day
+                                                          m - month
+                                                          y - year
+                                                          para o ordem 
+                                                          que quer  */
